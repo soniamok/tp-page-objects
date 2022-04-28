1c7919e6736edd8f6d94fe5655cc04f569396035
sonia mokaddem <soniamokaddem88@gmail.com>
2022-04-28 14:39:46 +0200

    first page object

--- test__amazon.py
+++ test__amazon.py
@@ -1,8 +1,21 @@
+from time import sleep
 from selenium import webdriver
+from page_objects.HomePage import HomePage
 
 
 def test__amazon1():
     driver = webdriver.Chrome()
     driver.get("https://www.amazon.fr/")
     driver.maximize_window()
+    driver.quit()
+
+
+def test__page__object():
+    driver = webdriver.Chrome()
+    driver.get("https://www.amazon.fr/")
+    homePage = HomePage(driver)
+    homePage.closeCookiesPopup()
+    homePage.openAllMenu()
+    homePage.openBookCategory()
+    homePage.openAllBooks()
     driver.quit()
